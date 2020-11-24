44b923fa50916d397ff071ae0f0db7bf271f07d4
Ken Hasenbank <Ken.Hasenbank@mhp.com>
2020-11-25 00:42:03 +0100

    update bed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -780,16 +780,16 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 200
+#define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -12
-#define Z_MIN_POS 0
+#define Y_MIN_POS -20
+#define Z_MIN_POS -2
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 220
+#define Z_MAX_POS 200
 
 // If enabled, axes won't move below MIN_POS in response to movement commands.
 #define MIN_SOFTWARE_ENDSTOPS
